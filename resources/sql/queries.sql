34ea5abc2c43c207593dc4c28a3c96ac9188f78d
sandmark <sandmark.m@gmail.com>
2019-02-22 08:31:10 +0900

    Add queries that does save/get message(s) on guestbook

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -19,3 +19,13 @@ WHERE id = :id
 -- :doc deletes a user record given the id
 DELETE FROM users
 WHERE id = :id
+
+-- :name save-message! :! :n
+-- :doc creates a new message
+INSERT INTO guestbook
+            (name, message, timestamp)
+VALUES (:name, :message, :timestamp)
+
+-- :name get-messages :? :*
+-- :doc selects all available messages
+SELECT * FROM guestbook
