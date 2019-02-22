8287e184f71df45652ac056bc3c93f4ccfe3239c
sandmark <sandmark.m@gmail.com>
2019-02-22 09:51:36 +0900

    Add features message-index and message-save

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -6,13 +6,38 @@
             [ring.util.http-response :as response]
             [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+
+   [:message
+    st/required
+    st/string
+    {:message  "message must contain at least 10 characters"
+     :validate #(> (count %) 9)}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do (db/save-message!
+         (assoc params :timestamp (java.util.Date.)))
+        (response/found "/"))))
+
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render request
+                 "home.html"
+                 (merge {:messages (db/get-messages)}
+                        (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
 (defroutes home-routes
   (GET "/" request (home-page request))
+  (POST "/" request (save-message! request))
   (GET "/about" request (about-page request)))
-
