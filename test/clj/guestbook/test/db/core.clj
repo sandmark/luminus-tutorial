55f9ad6549b575002a1e21f6f4fab6fade8e81fd
sandmark <sandmark.m@gmail.com>
2019-02-22 11:14:31 +0900

    Add basic tests

--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -1,36 +1,34 @@
 (ns guestbook.test.db.core
-  (:require [guestbook.db.core :refer [*db*] :as db]
-            [luminus-migrations.core :as migrations]
-            [clojure.test :refer :all]
+  (:require [java-time :refer [local-date-time]]
             [clojure.java.jdbc :as jdbc]
+            [clojure.test :refer :all]
             [guestbook.config :refer [env]]
+            [guestbook.db.core :as db :refer [*db*]]
+            [luminus-migrations.core :as migrations]
             [mount.core :as mount]))
 
 (use-fixtures
   :once
   (fn [f]
     (mount/start
-      #'guestbook.config/env
-      #'guestbook.db.core/*db*)
+     #'guestbook.config/env
+     #'guestbook.db.core/*db*)
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
 (deftest test-users
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+    (let [timestamp (local-date-time)]
+      (is (= 1 (db/save-message!
+                t-conn
+                {:name      "Bob"
+                 :message   "Hello, World"
+                 :timestamp timestamp}
+                {:connection t-conn})))
+      (is (= {:name      "Bob"
+              :message   "Hello, World"
+              :timestamp timestamp}
+             (-> (db/get-messages t-conn {})
+                 first
+                 (select-keys [:name :message :timestamp])))))))
